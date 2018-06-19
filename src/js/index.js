67139e6bfa15d3caf2d38325da1872c4f26ed20b
Jordan Walker <jwalker2276@gmail.com>
2018-06-18 21:46:49 -0500

    Added two quick methods to fetch and filter data.

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+function decode() {
+  const url = "https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/1G1ND52J9Y6186734?format=json";
+
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) throw new Error(error);
+      return res.json();
+    })
+    .then((myJSON) => filterData(myJSON))
+    .catch((error) => console.log("Error: ", error));
+}
+
+function filterData(data) {
+  const info = data.Results;
+  const arr = info.filter(function (element, index, array) {
+    return element.Value !== null;
+  }).filter(function (element, index, array) {
+    return element.Value !== "";
+  });
+  console.log(arr);
+}
